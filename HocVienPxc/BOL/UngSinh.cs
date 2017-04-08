fb7336f88a1e19110f4088f6ec72f5ae3b13234d
Jos. Đinh Ngọc Sơn <ad.nspro@gmail.com>
2017-04-08 16:07:30 +0700

    update UngSinh

--- HocVienPxc/BOL/UngSinh.cs
+++ HocVienPxc/BOL/UngSinh.cs
@@ -17,7 +17,7 @@ namespace HocVienPxc.BOL
         DateTime _ngaySinh;
         string _noiSinh;
         string _nguyenQuan;
-        string _hoKhau;
+        string _hoKhauThuongTru;
         string _soCMND;
         DateTime _ngayCapCMND;
         string _noiCapCMND;
@@ -37,5 +37,151 @@ namespace HocVienPxc.BOL
         string _nhanDinhOnGoi;
         string _sucKhoe;
         string _nhanDinhDiem;
+        public int MaUngSing
+        {
+            get { return _maUngSinh; }
+            set { _maUngSinh = value; }
+        }
+        public string TenThanh
+        {
+            get { return _tenThanh; }
+            set { _tenThanh = value; }
+        }
+        public string HoVaTenLot
+        {
+            get { return _hoVaTenLot; }
+            set { _hoVaTenLot = value; }
+        }
+        public string TenUngSing
+        {
+            get { return _tenUngSinh; }
+            set { _tenUngSinh = value; }
+        }
+        public int MaTinhTrang
+        {
+            get { return _maTinhTrang; }
+            set { _maTinhTrang = value; }
+        }
+        public int MaLop
+        {
+            get { return _maLop; }
+            set { _maLop = value; }
+        }
+        public DateTime NgaySinh
+        {
+            get { return _ngaySinh; }
+            set { _ngaySinh = value; }
+        }
+        public string NoiSinh
+        {
+            get { return _noiSinh; }
+            set { _noiSinh = value; }
+        }
+        public string NguyenQuan
+        {
+            get { return _nguyenQuan; }
+            set { _nguyenQuan = value; }
+        }
+        public string HoKhauThuongTru
+        {
+            get { return _hoKhauThuongTru; }
+            set { _hoKhauThuongTru = value; }
+        }
+        public string SoCMND
+        {
+            get { return _soCMND; }
+            set { _soCMND = value; }
+        }
+        public DateTime NgayCapCMND
+        {
+            get { return _ngayCapCMND; }
+            set { _ngayCapCMND = value; }
+        }
+        public string NoiCapCMND
+        {
+            get { return _noiCapCMND; }
+            set { _noiCapCMND = value; }
+        }
+        public DateTime NgayRuaToi
+        {
+            get { return _ngayRuaToi; }
+            set { _ngayRuaToi = value; }
+        }
+        public string GiaoXuRuaToi
+        {
+            get { return _giaoXuRuaToi; }
+            set { _giaoXuRuaToi = value; }
+        }
+        public DateTime NgayThemSuc
+        {
+            get { return _ngayThemSuc; }
+            set { _ngayThemSuc = value; }
+        }
+        public string GiaoXuThemSuc
+        {
+            get { return _giaoXuThemSuc; }
+            set { _giaoXuThemSuc = value; }
+        }
+        public string GiaoXu
+        {
+            get { return _giaoXu; }
+            set { _giaoXu = value; }
+        }
+        public string GiaoPhan
+        {
+            get { return _giaoPhan; }
+            set { _giaoPhan = value; }
+        }
+        public string DienThoaiGiaoXu
+        {
+            get { return _dienThoaiGiaoXu; }
+            set { _dienThoaiGiaoXu = value; }
+        }
+        public string DienThoaiCaNhan
+        {
+            get { return _dienThoaiCaNhan; }
+            set { _dienThoaiCaNhan = value; }
+        }
+        public string CaTinh
+        {
+            get { return _caTinh; }
+            set { _caTinh = value; }
+        }
+        public string LichSuOnGoi
+        {
+            get { return _lichSuOnGoi; }
+            set { _lichSuOnGoi = value; }
+        }
+        public string YThucDoiTu
+        {
+            get { return _yThucDoiTu; }
+            set { _yThucDoiTu = value; }
+        }
+        public string HocTapNangKhieu
+        {
+            get { return _hocTapNangKhieu; }
+            set { _hocTapNangKhieu = value; }
+        }
+        public string NhungDiemCoGangThayDoi
+        {
+            get { return _nhungDiemCoGangThayDoi; }
+            set { _nhungDiemCoGangThayDoi = value; }
+        }
+        public string NhanDinhOnGoi
+        {
+            get { return _nhanDinhOnGoi; }
+            set { _nhanDinhOnGoi = value; }
+        }
+        public string SucKhoe
+        {
+            get { return _sucKhoe; }
+            set { _sucKhoe = value; }
+        }
+        public string NhanDinhDiem
+        {
+            get { return _nhanDinhDiem; }
+            set { _nhanDinhDiem = value; }
+        }
     }
+    
 }
--- HocVienPxc/DAL/UngSinhDAL.cs
+++ HocVienPxc/DAL/UngSinhDAL.cs
@@ -6,10 +6,76 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using System.Collections.ObjectModel;
+using HocVienPxc.BOL;
 
 namespace HocVienPxc.DAL
 {
-    class UngSinhDAL
+    public class UngSinhDAL:BaseDAL
     {
+        public UngSinh UngSinhIDataReader(IDataReader Reader)
+        {
+            UngSinh obj = new UngSinh();
+            obj.MaUngSing = (Reader["MaUngSinh"] is DBNull) ? int.MinValue : (int)Reader["MaUngSinh"];
+            obj.TenThanh = (Reader["TenThanh"] is DBNull) ? string.Empty : (string)Reader["TenThanh"];
+            obj.HoVaTenLot = (Reader["HoVaTenLot"] is DBNull) ? string.Empty : (string)Reader["HoVaTenLot"];
+            obj.TenUngSing = (Reader["TenUngSinh"] is DBNull) ? string.Empty : (string)Reader["TenUngSinh"];
+            obj.MaTinhTrang = (Reader["MaTinhTrang"] is DBNull) ? int.MinValue : (int)Reader["MaTinhTrang"];
+            obj.MaLop = (Reader["MaLop"] is DBNull) ? int.MinValue : (int)Reader["MaLop"];
+            obj.NgaySinh = (Reader["NgaySinh"] is DBNull) ? DateTime.MinValue : (DateTime)Reader["NgaySinh"];
+            obj.NoiSinh = (Reader["NoiSinh"] is DBNull) ? string.Empty : (string)Reader["NoiSinh"];
+            obj.NguyenQuan = (Reader["NguyenQuan"] is DBNull) ? string.Empty : (string)Reader["NguyenQuan"];
+            obj.HoKhauThuongTru = (Reader["HoKhauThuongTru"] is DBNull) ? string.Empty : (string)Reader["HoKhauThuongTru"];
+            obj.SoCMND = (Reader["SoCMND"] is DBNull) ? string.Empty : (string)Reader["SoCMND"];
+            obj.NgayCapCMND = (Reader["NgayCapCMND"] is DBNull) ? DateTime.MinValue : (DateTime)Reader["NgayCapCMND"];
+            obj.NoiCapCMND = (Reader["NoiCapCMND"] is DBNull) ? string.Empty : (string)Reader["NoiCapCMND"];
+            obj.NgayRuaToi = (Reader["NgayRuaToi"] is DBNull) ? DateTime.MinValue : (DateTime)Reader["NgayRuaToi"];
+            obj.GiaoXuRuaToi = (Reader["GiaoXuRuaToi"] is DBNull) ? string.Empty : (string)Reader["GiaoXuRuaToi"];
+            obj.NgayThemSuc = (Reader["NgayThemSuc"] is DBNull) ? DateTime.MinValue : (DateTime)Reader["NgayThemSuc"];
+            obj.GiaoXuThemSuc = (Reader["GiaoXuThemSuc"] is DBNull) ? string.Empty : (string)Reader["GiaoXuThemSuc"];
+            obj.GiaoXu = (Reader["GiaoXu"] is DBNull) ? string.Empty : (string)Reader["GiaoXu"];
+            obj.GiaoPhan = (Reader["GiaoPhan"] is DBNull) ? string.Empty : (string)Reader["GiaoPhan"];
+            obj.DienThoaiGiaoXu = (Reader["DienThoaiGiaoXu"] is DBNull) ? string.Empty : (string)Reader["DienThoaiGiaoXu"];
+            obj.DienThoaiCaNhan = (Reader["DienThoaiCaNhan"] is DBNull) ? string.Empty : (string)Reader["DienThoaiCaNhan"];
+            obj.CaTinh = (Reader["CaTinh"] is DBNull) ? string.Empty : (string)Reader["CaTinh"];
+            obj.LichSuOnGoi = (Reader["LichSuOnGoi"] is DBNull) ? string.Empty : (string)Reader["LichSuOnGoi"];
+            obj.YThucDoiTu = (Reader["YThucDoiTu"] is DBNull) ? string.Empty : (string)Reader["YThucDoiTu"];
+            obj.HocTapNangKhieu = (Reader["HocTapNangKhieu"] is DBNull) ? string.Empty : (string)Reader["HocTapNangKhieu"];
+            obj.NhungDiemCoGangThayDoi = (Reader["NhungDiemCoGangThayDoi"] is DBNull) ? string.Empty : (string)Reader["NhungDiemCoGangThayDoi"];
+            obj.NhanDinhOnGoi = (Reader["NhanDinhOnGoi"] is DBNull) ? string.Empty : (string)Reader["NhanDinhOnGoi"];
+            obj.NhanDinhDiem = (Reader["NhanDinhDiem"] is DBNull) ? string.Empty : (string)Reader["NhanDinhDiem"];
+
+            return obj;
+        }
+        public int ThemTaiKhoan(UngSinh obj)
+        {
+            try
+            {
+                using (SqlConnection conn = getConnect())
+                {
+
+                    conn.Open();
+                    string q = "Insert into UngSinh values (N'" + obj.TenThanh + "',N'" + obj.HoVaTenLot + "',N'" + obj.TenUngSing + "',N'" + obj.MaTinhTrang + "',N'" + obj.MaLop + "',N'" + obj.NgaySinh + "',N'" + obj.NoiSinh + "',N'" + obj.NguyenQuan + "',N'" + obj.HoKhauThuongTru + "',N'" + obj.SoCMND + "',N'" + obj.NgayCapCMND + "',N'" + obj.NoiCapCMND + "',N'" + obj.NgayRuaToi + "',N'" + obj.GiaoXuRuaToi + "',N'" + obj.NgayThemSuc + "',N'" + obj.GiaoXuThemSuc + "',N'" + obj.GiaoXu + "',N'" + obj.GiaoPhan + "',N'" + obj.DienThoaiGiaoXu + "',N'" + obj.DienThoaiCaNhan + "',N'" + obj.CaTinh + "',N'" + obj.LichSuOnGoi + "',N'" + obj.YThucDoiTu + "',N'" + obj.HocTapNangKhieu + "',N'" + obj.NhungDiemCoGangThayDoi + "',N'" + obj.NhanDinhOnGoi + "',N'" + obj.SucKhoe + "',N'" + obj.NhanDinhDiem + "') ";
+                    SqlCommand myCommand = new SqlCommand(q, conn);
+                    myCommand.CommandType = CommandType.Text;
+                    myCommand.ExecuteNonQuery();
+                    conn.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        public ObservableCollection<UngSinh> HienThiTatCa()
+        {
+            using (SqlConnection conn = getConnect())
+            {
+                conn.Open();
+                SqlCommand myCommand = new SqlCommand("Select * from UngSinh",conn);
+                myCommand.CommandType = CommandType.Text;
+                //ObservableCollection
+            }
+        }
     }
 }
