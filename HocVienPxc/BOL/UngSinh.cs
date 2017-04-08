8e9e8660edcfd317cd8691978102fe6a6857d7bc
Jos. Đinh Ngọc Sơn <ad.nspro@gmail.com>
2017-04-08 14:56:06 +0700

    1

---
+++ HocVienPxc/BOL/UngSinh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HocVienPxc.BOL
+{
+    public class UngSinh
+    {
+        int _maUngSinh;
+        string _tenThanh;
+        string _hoVaTenLot;
+        string _tenUngSinh;
+        int _maTinhTrang;
+        int _maLop;
+        
+    }
+}
---
+++ HocVienPxc/DAL/BaseDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HocVienPxc.DAL
+{
+    public class BaseDAL
+    {
+        public SqlConnection getConnect()
+        {
+            // su dung server tren may son 
+            //return new SqlConnection(@"Data Source=DESKTOP-GUS3GC7\SQLEXPRESS;Initial Catalog=AspirantHouseSoft;Integrated Security=True;");
+            return new SqlConnection(@"Data Source=Data Source=192.168.1.29;Initial Catalog=AspirantHouseSoft;Persist Security Info=True;User ID=sa; Password=bonaventura");
+        }
+        public DataTable GetDataTable(string sql)
+        {
+            SqlConnection conn = getConnect();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public void ExcuteNonQuery(string sql)
+        {
+            SqlConnection conn = getConnect();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+    }
+}
