28a17bc3ad5f147dc7b7b7e883f3b2d050fd9d9c
Jos. Đinh Ngọc Sơn <ad.nspro@gmail.com>
2017-04-08 15:11:35 +0700

    Revert "1"
    This reverts commit fb10d950940a416735c35cbc4962b2e43f551447.

--- HocVienPxc/BOL/UngSinh.cs
+++ HocVienPxc/BOL/UngSinh.cs
@@ -14,21 +14,6 @@ namespace HocVienPxc.BOL
         string _tenUngSinh;
         int _maTinhTrang;
         int _maLop;
-        DateTime _ngaySinh;
-        string _noiSinh;
-        string _nguyenQuan;
-        string _hoKhau;
-        string _soCMND;
-        DateTime _ngayCapCMND;
-        string _noiCapCMND;
-        DateTime _ngayRuaToi;
-        string _giaoXuRuaToi;
-        DateTime _ngayThemSuc;
-        string _giaoXuThemSuc;
-        string _giaoXu;
-        string _giaoPhan;
-        string _dienThoaiGiaoXu;
-        string _noiSinh;
-
+        
     }
 }
