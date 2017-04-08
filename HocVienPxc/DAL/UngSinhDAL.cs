56227a3e324b4d188866b297012faa9870682395
Jos. Đinh Ngọc Sơn <ad.nspro@gmail.com>
2017-04-08 15:12:29 +0700

    lll

--- HocVienPxc/DAL/UngSinhDAL.cs
+++ HocVienPxc/DAL/UngSinhDAL.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
 
 namespace HocVienPxc.DAL
 {
