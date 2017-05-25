b12febf426951372f364f146b87c798f977e6c92
DEV1 <DEV1@ASPH-DEV1>
2017-05-25 17:08:48 +0700

    Up THEM LOP

--- HocVienPxc/Form/frmTongQuan.xaml.cs
+++ HocVienPxc/Form/frmTongQuan.xaml.cs
@@ -24,53 +24,6 @@ namespace HocVienPxc.Form
             InitializeComponent();
         }
 
-
-
-        ////Cấu hình
-        //private void stp_Menu_CauHinh_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //    stp_Menu_CauHinh.Background = Brushes.DimGray;
-        //}
-
-        //private void stp_Menu_CauHinh_MouseLeave(object sender, MouseEventArgs e)
-        //{
-        //    stp_Menu_CauHinh.Background = Brushes.Transparent;
-        //}
-
-        //private void stp_Menu_CauHinh_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    stp_Menu_CauHinh.Background = Brushes.DarkSlateGray;
-
-        //}
-
-        //private void stp_Menu_CauHinh_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        //{
-        //    stp_Menu_CauHinh.Background = Brushes.Transparent;
-        //}
-
-
-
-        //Danh sách ứng sinh
-        //private void stp_Menu_DanhSachUngSinh_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //    stp_Menu_DanhSachUngSinh.Background = Brushes.DimGray;
-        //}
-
-        //private void stp_Menu_DanhSachUngSinh_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    stp_Menu_DanhSachUngSinh.Background = Brushes.DarkSlateGray;
-        //}
-
-        //private void stp_Menu_DanhSachUngSinh_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        //{
-        //    stp_Menu_DanhSachUngSinh.Background = Brushes.Transparent;
-        //}
-
-        //private void stp_Menu_DanhSachUngSinh_MouseLeave(object sender, MouseEventArgs e)
-        //{
-        //    stp_Menu_DanhSachUngSinh.Background = Brushes.Transparent;
-        //}
-
         ////Đăng xuất
         private void stp_Menu_DangXuat_MouseMove(object sender, MouseEventArgs e)
         {
